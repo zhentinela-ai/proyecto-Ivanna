a21ec96e674d3634ac8ae1fb65bd21ef55fc3737
Zhent <jdcaballeror@correo.udistrital.edu.co>
2022-11-17 23:57:13 -0500

    before of create the function for sections

--- GabrielaAvila_eje2/mapa.js
+++ GabrielaAvila_eje2/mapa.js
@@ -5,21 +5,137 @@ export class Mapa extends Phaser.Scene {
 
   preload() {
     this.load.image("background", "map_pictures/detroit-lakes.png");
-    this.load.image("lake", "map_pictures/lake1.png");
-    this.load.image("urban", "map_pictures/urban1.png");
+    this.load.image("lake", "map_pictures/lake.png");
+    this.load.image("lake_hover", "map_pictures/lake_hover.png");
+    this.load.image("urban", "map_pictures/urban.png");
+    this.load.image("urban_hover", "map_pictures/urban_hover.png");
+    this.load.image("natural", "map_pictures/natural.png");
+    this.load.image("natural_hover", "map_pictures/natural_hover.png");
+    this.load.image("aves", "map_pictures/aves.png");
+    this.load.image("aves_hover", "map_pictures/aves_hover.png");
   }
 
   create() {
     this.background = this.add.tileSprite(800, 800, 1600, 1600, "background");
     this.background.setTileScale(0.245, 0.245);
 
-    // this.lake = this.add.sprite(310, 432, "lake");
-    this.lake = this.add.sprite(300, 300, "lake");
-    this.lake.setScale(1, 1);
-    this.lake.transparent = true;
+    const createSection = (x, y, key, key_hover, scene) => {
+      const section = this.add.image(x, y, key).setInteractive();
+      section.on("pointerover", () => {
+        section.setTexture(key_hover);
+      });
+      section.on("pointerout", () => {
+        section.setTexture(key);
+      });
+      section.on("pointerdown", () => {
+        this.scene.start(scene);
+      });
+    };
 
-    // this.urban = this.add.sprite(295, 316, "urban");
-    this.urban = this.add.sprite(300, 300, "urban");
+    createSection(100, 100, "lake", "lake_hover", "lake");
+    
+    this.urban = this.add.sprite(222, 208, "urban").setInteractive();
+    
+    this.urbanHover = this.add.sprite(222, 208, "urban_hover").setInteractive();
+    this.urbanHover.setAlpha(0);
+
+    this.urban.on("pointerover", async (event) => {
+      for (let i = 0; i <= 100; i++) {
+        this.urbanHover.setAlpha(i / 100);
+        await sleep(3);
+        this.urbanHover.scaleX += 0.00035;
+        this.urbanHover.scaleY += 0.00035;
+      }
+    });
+    this.urbanHover.on("pointerout", async (event) => {
+      for (let i = 0; i <= 100; i++) {
+        this.urbanHover.setAlpha(1 - i / 100);
+        await sleep(3);
+        this.urbanHover.scaleX -= 0.00035;
+        this.urbanHover.scaleY -= 0.00035;
+      }
+    });
+
+    // this.lake = this.add.sprite(317, 432, "lake").setInteractive();
+
+    // this.lakeHover = this.add.sprite(317, 432, "lake_hover").setInteractive();
+    // this.lakeHover.setAlpha(0);
+
+    // function sleep(ms) {
+    //   return new Promise((resolve) => setTimeout(resolve, ms));
+    // }
+
+    // this.lake.on("pointerover", async (event) => {
+    //   for (let i = 0; i <= 100; i++) {
+    //     this.lakeHover.setAlpha(i / 100);
+    //     await sleep(3);
+    //     this.lakeHover.scaleX += 0.0005;
+    //     this.lakeHover.scaleY += 0.0005;
+    //   }
+    // });
+    // this.lakeHover.on("pointerout", async (event) => {
+    //   for (let i = 0; i <= 100; i++) {
+    //     this.lakeHover.setAlpha(1 - i / 100);
+    //     await sleep(3);
+    //     this.lakeHover.scaleX -= 0.0005;
+    //     this.lakeHover.scaleY -= 0.0005;
+    //   }
+    // });
+
+    this.natural = this.add.sprite(456, 130, "natural").setInteractive();
+
+    this.naturalHover = this.add
+      .sprite(456, 130, "natural_hover")
+      .setInteractive();
+    this.naturalHover.setAlpha(0);
+
+    this.naturalHover = this.add
+      .sprite(456, 130, "natural_hover")
+      .setInteractive();
+    this.naturalHover.setAlpha(0);
+
+    this.natural.on("pointerover", async (event) => {
+      for (let i = 0; i <= 100; i++) {
+        this.naturalHover.setAlpha(i / 100);
+        await sleep(3);
+        this.naturalHover.scaleX += 0.0005;
+        this.naturalHover.scaleY += 0.0005;
+      }
+    });
+
+    this.naturalHover.on("pointerout", async (event) => {
+      for (let i = 0; i <= 100; i++) {
+        this.naturalHover.setAlpha(1 - i / 100);
+        await sleep(3);
+        this.naturalHover.scaleX -= 0.0005;
+        this.naturalHover.scaleY -= 0.0005;
+      }
+    });
+
+    this.aves = this.add.sprite(215, 490, "aves").setInteractive();
+
+    this.avesHover = this.add.sprite(215, 490, "aves_hover").setInteractive();
+    this.avesHover.setAlpha(0);
+
+    this.aves.on("pointerover", async (event) => {
+      for (let i = 0; i <= 100; i++) {
+        this.avesHover.setAlpha(i / 100);
+        await sleep(3);
+        this.avesHover.scaleX += 0.0005;
+        this.avesHover.scaleY += 0.0005;
+      }
+    });
+
+    this.avesHover.on("pointerout", async (event) => {
+      for (let i = 0; i <= 100; i++) {
+        this.avesHover.setAlpha(1 - i / 100);
+        await sleep(3);
+        this.avesHover.scaleX -= 0.0005;
+        this.avesHover.scaleY -= 0.0005;
+      }
+    });
+
+    // this.urban = this.add.sprite(300, 300, "urban");
 
     // this.gameoverImage.visible = false;
 
